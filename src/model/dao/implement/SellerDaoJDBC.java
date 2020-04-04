216a86f628a9a85355aeea872cc1f98437459e5d
Junior Mendes <juniormendesjp@gmail.com>
2020-04-04 17:21:07 -0300

    findById implementation

--- src/model/dao/implement/SellerDaoJDBC.java
+++ src/model/dao/implement/SellerDaoJDBC.java
@@ -1,11 +1,24 @@
 package model.dao.implement;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
 import java.util.List;
 
+import db.DB;
+import db.DBException;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class SellerDaoJDBC implements SellerDao {
+	private Connection conexao;
+	
+	public SellerDaoJDBC(Connection conexao) {
+		this.conexao = conexao;
+	}
 
 	@Override
 	public void insert(Seller seller) {
@@ -27,13 +40,55 @@ public class SellerDaoJDBC implements SellerDao {
 
 	@Override
 	public Seller findById(Integer id) {
-		// TODO Auto-generated method stub
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		SimpleDateFormat sdf= new SimpleDateFormat("dd/MM/yyyy");
+		
+		try {
+			st =conexao.prepareStatement(
+					"SELECT seller.*,department.Name as DepName "
+					+ "FROM seller INNER JOIN department "
+					+ "ON seller.DepartmentId = department.Id "
+					+ "WHERE seller.Id = ?");
+			
+			st.setInt(1, id);
+			rs = st.executeQuery();
+			
+			
+			if (rs.next()) {
+				Department departamento = new Department();
+				departamento.setId(rs.getInt("DepartmentId"));
+				departamento.setName(rs.getString("DepName"));
+				
+				Seller seller = new Seller();
+				seller.setId(rs.getInt("Id"));
+				seller.setName(rs.getString("Name"));
+				seller.setEmail(rs.getString("Email"));
+				seller.setBaseSalary(rs.getDouble("BaseSalary"));
+				seller.setBirthDate(rs.getDate("BirthDate"));
+				seller.setDepartment(departamento);
+				
+				return seller;
+			}
+			else {
+				System.out.println("Nenhum vendedor encontrado");
+			}
+		} catch (SQLException e) {
+			throw new DBException(e.getMessage());
+		}
+		
+		finally {
+			DB.closeResultSet(rs);
+			DB.closeStatement(st);
+		}
 		return null;
+		
+	
 	}
 
 	@Override
 	public List<Seller> findAll() {
-		// TODO Auto-generated method stub
+
 		return null;
 	}
 
